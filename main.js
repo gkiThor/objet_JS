426ff6003ed2e801b704efb2edf48d76781035d5
Guillaume Soulichanh <g.soulichanh@gmail.com>
2024-08-29 18:36:06 +0200

    La classe String

--- main.js
+++ main.js
@@ -1,11 +1,17 @@
-console.log(Math.PI); // constante 3.14
-console.log(Math.random()); // nombre aleatoire
-console.log(Math.ceil(3.1)); // 4 arrondi strictement superieur
-console.log(Math.floor(3.9)); // 3 arrondi au plancher
-console.log(Math.round(3.4)); // 3 arrondi au demi point superieur
-console.log(Math.round(3.5)); // 4 arrondi au demi point superieur
-console.log(Math.min(12, 8, 13)); // 8 donne le plus petit nombre dans la suite de nombre
-console.log(Math.max(12, 8, 13)); // 13 donne le plus grand nombre dans la suite de nombre
-console.log(Math.abs(-3)); // 3 donne la valeur absolue
-console.log(Math.abs(3)); // 3 donne la valeur absolue
-console.log(Math.sqrt(36)); // 6 racine carree
+console.log("MDN la référence de Javascript!".length); // la propriété length donne le nombre de caractère
+console.log("MDN la référence de Javascript!".charAt(2)); // la methode charAt() donne la position du caractère 
+console.log("MDN la référence de Javascript!".indexOf("de"));//La méthode indexOf() donne l'index de la première occurrence d'un tableau ou dans une chaîne de caractères
+console.log("MDN la référence de Javascript!".split(' '));// La méthode split() transforme une chaine de caractère en tableau
+console.log("MDN la référence de Javascript!".substr(4));// La méthode substr() extrait une chaine de caractère à une position ** obsolète **
+console.log("MDN la référence de Javascript!".substr(4,12));// La méthode substr() extrait une chaine de caractère à une position et le nombre de caractere ** obsolète **
+console.log("MDN la référence de Javascript!".slice(4,16));// La méthode slice() extrait une chaine de caractère à une position de depart et un point de fin
+console.log("MDN la référence de Javascript!".toUpperCase());// La méthode toUpperCase() met en majuscule une chaine
+
+// Ne pas employer le constructeur String()!
+let s1 = "MDN";
+let s2 = new String("MDN");
+console.log(s1 == s2);
+console.log(s1 === s2);
+console.log(typeof s1); // string (type primitif)
+console.log(typeof s2); // object (instance de String)
+
