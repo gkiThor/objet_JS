ab0ef2de4cca5dbc077f4555da6a7596145950e7
Guillaume Soulichanh <g.soulichanh@gmail.com>
2024-08-29 18:00:05 +0200

    La classe Math

--- main.js
+++ main.js
@@ -1,28 +1,11 @@
-// Une classe
-class Stylo{
-
-    constructor(marque){
-        this.marque = marque;
-
-        if(Stylo.nb === undefined){
-            Stylo.nb = 0;
-        }
-        Stylo.nb++;
-    }
-
-    ecrire(){
-        console.log(`Le stylo ${this.marque} écrit`);
-    }
-
-    // Méthode statique
-    static denombrer(){
-        console.log(`Il y a ${Stylo.nb} stylos`);
-    }
-}
-
-// Programme principal
-new Stylo("Bic");
-new Stylo("Pentel");
-
-console.log(Stylo.nb);
-Stylo.denombrer();
+console.log(Math.PI); // constante 3.14
+console.log(Math.random()); // nombre aleatoire
+console.log(Math.ceil(3.1)); // 4 arrondi strictement superieur
+console.log(Math.floor(3.9)); // 3 arrondi au plancher
+console.log(Math.round(3.4)); // 3 arrondi au demi point superieur
+console.log(Math.round(3.5)); // 4 arrondi au demi point superieur
+console.log(Math.min(12, 8, 13)); // 8 donne le plus petit nombre dans la suite de nombre
+console.log(Math.max(12, 8, 13)); // 13 donne le plus grand nombre dans la suite de nombre
+console.log(Math.abs(-3)); // 3 donne la valeur absolue
+console.log(Math.abs(3)); // 3 donne la valeur absolue
+console.log(Math.sqrt(36)); // 6 racine carree
