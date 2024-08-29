7c5aee0c299fb24b93acfe7694fe932874925189
Guillaume Soulichanh <g.soulichanh@gmail.com>
2024-08-29 17:49:46 +0200

    Les classes: membres statiques

--- main.js
+++ main.js
@@ -1,21 +1,28 @@
 // Une classe
 class Stylo{
+
     constructor(marque){
         this.marque = marque;
+
+        if(Stylo.nb === undefined){
+            Stylo.nb = 0;
+        }
+        Stylo.nb++;
     }
 
     ecrire(){
         console.log(`Le stylo ${this.marque} écrit`);
     }
+
+    // Méthode statique
+    static denombrer(){
+        console.log(`Il y a ${Stylo.nb} stylos`);
+    }
 }
 
 // Programme principal
-let stylo = new Stylo("Bic");
-console.log(stylo);
-stylo.ecrire();
+new Stylo("Bic");
+new Stylo("Pentel");
 
-// Possible mais dangeureux...
-stylo.couleur = "rouge";
-console.log(stylo);
-Stylo.prototype.poids = 50; // TOUS les stylos ont maintenant un poids de 50g
-console.log(stylo.poids); // même ceux deja créés !
+console.log(Stylo.nb);
+Stylo.denombrer();
