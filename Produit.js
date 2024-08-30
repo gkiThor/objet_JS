b863a1046618d0257785d0c33cfb0ff3530f4414
Guillaume Soulichanh <g.soulichanh@gmail.com>
2024-08-30 10:16:21 +0200

    application boulanger

---
+++ Produit.js
@@ -0,0 +1,7 @@
+class Produit{
+
+    constructor(nom){
+        this.nom = nom;
+        console.log(`${this.nom} est un produit`);
+    }
+}
--- main.js
+++ main.js
@@ -1,32 +1,9 @@
-let tab = [4, 2, 1, 3];
-
-console.log(tab.length); // 4
-tab.pop(); // Extraire de la fin
-console.log(tab); // [4, 2, 1]
-tab.push(3); // Ajouter à la fin
-console.log(tab); // [4, 2, 1, 3]
-tab.shift(); // Extraire du début
-console.log(tab); // [2, 1, 3]
-tab.unshift(4); // Ajouter au début
-console.log(tab); // [4, 2, 1, 3]
-console.log(tab.join('-')); // "4-2-1-3"
-tab.sort(); // Trier
-console.log(tab); // [1, 2, 3, 4]
-
-// Parcourir avec forEach()
-tab.forEach(function(e){
-	console.log(e);
-});
-
-// Traiter avec map()
-let tab2 = tab.map(function(e){
-	return e * 2;
-});
-console.log(tab2);
-
-// Filtrer avec filter()
-let tab3 = tab.filter(function (e) {
-	return e % 3 === 0;
-});
-console.log(tab3);
+let guillaume=new Boulanger("Guillaume");
+let baguette=new Produit("baguette",0.20,1.20);
+let croissant=new Produit("croissant",0.25,1.50);
+let painDeMie=new Produit("pain de mie",0.50,4.50);
+guillaume.fabriquer(baguette,80);
+guillaume.fabriquer(croissant,60);
+guillaume.fabriquer(painDeMie,20);
+guillaume.bilan();
 
